4b264136666b79bd8b9a8710daa12907a24ed42a
ramezyousri21 <53097570+ramezyousri21@users.noreply.github.com>
2020-03-19 14:45:31 +0200

    uploaded files

---
+++ precache-manifest.53063e5d92487ea46e4d4b4091afc1a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "423915bd85d3d79ac89298eed6ee1c32",
+    "url": "/avadeskoursdataapp/index.html"
+  },
+  {
+    "revision": "e75c92863dca9a0c09d0",
+    "url": "/avadeskoursdataapp/static/css/main.7e31679b.chunk.css"
+  },
+  {
+    "revision": "5dd9b7135e70392484c8",
+    "url": "/avadeskoursdataapp/static/js/2.ebb00f56.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/avadeskoursdataapp/static/js/2.ebb00f56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e75c92863dca9a0c09d0",
+    "url": "/avadeskoursdataapp/static/js/main.d74058d4.chunk.js"
+  },
+  {
+    "revision": "6036e5854b8a0341ad8a",
+    "url": "/avadeskoursdataapp/static/js/runtime-main.5d181faf.js"
+  }
+]);
